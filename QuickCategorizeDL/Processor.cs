7c5ee7d73110a672534939d00e4f922307b6e9c8
John J Schultz <johnmail@bitsofgenius.com>
2019-07-01 14:04:08 -0400

    misc code cleanup

--- QuickCategorizeDL/Processor.cs
+++ QuickCategorizeDL/Processor.cs
@@ -1,11 +1,7 @@
-﻿using System;
+﻿using BOG.Framework;
+using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
-using BOG.Framework;
 
 namespace QuickCategorizeDL
 {
